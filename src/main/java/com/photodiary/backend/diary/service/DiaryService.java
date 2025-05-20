1260b57ce20ec167ee6b36c4a261afc0fe8b7fb5
JangIkhwan <bluensky0213@gmail.com>
2025-05-20 14:44:40 +0900

    [Feat] #1 DiaryService 추가

---
+++ src/main/java/com/photodiary/backend/diary/service/DiaryService.java
@@ -0,0 +1,16 @@
+package com.photodiary.backend.diary.service;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class DiaryService {
+    public void createDiary(List<MultipartFile> files, boolean isPublic) {
+
+    }
+}
