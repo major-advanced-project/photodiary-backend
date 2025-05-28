7846eeaf0cfe8f20082fb52b951b6309933a68fa
JangIkhwan <bluensky0213@gmail.com>
2025-05-28 17:06:21 +0900

    [Feat] #1 Blip2ModelApi 클래스 추가

---
+++ src/main/java/com/photodiary/backend/diary/util/Blip2ModelApi.java
@@ -0,0 +1,13 @@
+package com.photodiary.backend.diary.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Component
+public class Blip2ModelApi {
+    public void retreiveImageDescirptions(List<MultipartFile> files) {
+
+    }
+}
