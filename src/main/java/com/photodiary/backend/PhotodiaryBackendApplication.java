004d078e69d1e66ee21cdd2ea9928d8ffc28761a
JangIkhwan <bluensky0213@gmail.com>
2025-06-04 15:52:26 +0900

    [Chore] #2 BaseEntity 구현

--- src/main/java/com/photodiary/backend/PhotodiaryBackendApplication.java
+++ src/main/java/com/photodiary/backend/PhotodiaryBackendApplication.java
@@ -2,8 +2,10 @@ package com.photodiary.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PhotodiaryBackendApplication {
 
     public static void main(String[] args) {
