17dc0dac52a0d1df4087db69e26de8e197b9aad5
JangIkhwan <bluensky0213@gmail.com>
2025-06-05 15:09:58 +0900

    [Feat] #4 서비스에 일기 상세 조회 메소드 추가

---
+++ src/main/java/com/photodiary/backend/diary/service/FindDiaryService.java
@@ -0,0 +1,25 @@
+package com.photodiary.backend.diary.service;
+
+import com.photodiary.backend.diary.dto.FindDiaryResponseDto;
+import com.photodiary.backend.diary.model.Diary;
+import com.photodiary.backend.diary.repository.DiaryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class FindDiaryService {
+    private final DiaryRepository diaryRepository;
+
+    public FindDiaryResponseDto findUserDiary(long userId, long diaryId) {
+        Optional<Diary> opt = diaryRepository.findByUserIdAndId(userId, diaryId);
+        if(opt.isEmpty()){
+            throw new RuntimeException("일기가 존재하지 않습니다");
+        }
+        return FindDiaryResponseDto.entityToDto(opt.get());
+    }
+}
