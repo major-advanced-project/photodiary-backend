bc6111203a8781ff97a6d026b64c506f4ca9ecdd
JangIkhwan <bluensky0213@gmail.com>
2025-06-05 14:24:13 +0900

    [Feat] #2 일기 레포티토리를 빈으로 추가

--- src/main/java/com/photodiary/backend/diary/repository/DiaryRepository.java
+++ src/main/java/com/photodiary/backend/diary/repository/DiaryRepository.java
@@ -2,9 +2,11 @@ package com.photodiary.backend.diary.repository;
 
 import com.photodiary.backend.diary.model.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
      Optional<Diary> findByUserIdAndId(long userId, long diaryId);
 }
