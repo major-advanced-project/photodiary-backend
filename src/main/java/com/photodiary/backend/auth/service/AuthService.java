cd785ee7a21af46a7e4b0eb673c4267990b9e82a
JangIkhwan <bluensky0213@gmail.com>
2025-06-19 16:41:20 +0900

    [Feat] #5 로그인 처리 로직 수정

--- src/main/java/com/photodiary/backend/auth/service/AuthService.java
+++ src/main/java/com/photodiary/backend/auth/service/AuthService.java
@@ -2,6 +2,8 @@ package com.photodiary.backend.auth.service;
 
 import com.photodiary.backend.auth.dto.LoginRequestDto;
 import com.photodiary.backend.auth.dto.LoginResponseDto;
+import com.photodiary.backend.global.exception.InvalidUserPasswordExecption;
+import com.photodiary.backend.global.exception.UserNotFoundException;
 import com.photodiary.backend.global.jwt.JwtUtil;
 import com.photodiary.backend.user.model.User;
 import com.photodiary.backend.user.repository.UserRepository;
@@ -17,14 +19,17 @@ import java.util.Optional;
 public class AuthService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
-    public LoginResponseDto login(LoginRequestDto request){
+    public LoginResponseDto login(LoginRequestDto request) {
         Optional<User> optUser = userRepository.findByEmail(request.email());
-        if(optUser.isPresent()) {
-            User foundUser = optUser.get();
-            if (request.password().equals(foundUser.getPassword())) {
-                return new LoginResponseDto(true, jwtUtil.generateAccessToken());
-            }
-        }                  
-        return new LoginResponseDto(false, null);
+        if(optUser.isEmpty()) {
+            throw new UserNotFoundException("존재하지 않는 유저입니다");
+        }
+
+        User foundUser = optUser.get();
+        if (!request.password().equals(foundUser.getPassword())) {
+            throw new InvalidUserPasswordExecption("비밀번호가 일치하지 않습니다");
+        }
+
+        return new LoginResponseDto(jwtUtil.generateAccessToken(foundUser.getId(), foundUser.getEmail()));
     }
 }
