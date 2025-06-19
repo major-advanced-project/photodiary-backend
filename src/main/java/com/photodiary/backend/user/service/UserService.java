c6919c46719d060a85a3423a44c146c19305faa8
JangIkhwan <bluensky0213@gmail.com>
2025-06-19 17:09:35 +0900

    [Feat] #5 UserService에 signup 메소드 추가

--- src/main/java/com/photodiary/backend/user/service/UserService.java
+++ src/main/java/com/photodiary/backend/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.photodiary.backend.user.service;
 
+import com.photodiary.backend.user.dto.SignupRequestDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class UserService {
+    public void signup(SignupRequestDto request) {
 
+    }
 }
