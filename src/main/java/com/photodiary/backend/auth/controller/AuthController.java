4e61b900570b951e2e8da61852d8eb633a5e2c62
JangIkhwan <bluensky0213@gmail.com>
2025-06-19 14:08:11 +0900

    [Feat] #5 login메소드 구현

--- src/main/java/com/photodiary/backend/auth/controller/AuthController.java
+++ src/main/java/com/photodiary/backend/auth/controller/AuthController.java
@@ -1,9 +1,23 @@
 package com.photodiary.backend.auth.controller;
 
+import com.photodiary.backend.auth.dto.LoginRequestDto;
+import com.photodiary.backend.auth.dto.LoginResponseDto;
+import com.photodiary.backend.auth.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequestMapping("auth")
 @RestController
 public class AuthController {
+    private final AuthService authService;
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto request){
+        log.info("[login]");
+        return ResponseEntity.ok(authService.login(request));
+    }
 }
