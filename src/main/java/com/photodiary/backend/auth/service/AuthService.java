01c164817ce4d6cb1c2b23fb05aa7f58410f3a09
JangIkhwan <bluensky0213@gmail.com>
2025-06-19 14:42:47 +0900

    [Feat] #5 login 처리 로직 구현

--- src/main/java/com/photodiary/backend/auth/service/AuthService.java
+++ src/main/java/com/photodiary/backend/auth/service/AuthService.java
@@ -1,10 +1,30 @@
 package com.photodiary.backend.auth.service;
 
+import com.photodiary.backend.auth.dto.LoginRequestDto;
+import com.photodiary.backend.auth.dto.LoginResponseDto;
+import com.photodiary.backend.global.jwt.JwtUtil;
+import com.photodiary.backend.user.model.User;
+import com.photodiary.backend.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class AuthService {
-
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    public LoginResponseDto login(LoginRequestDto request){
+        Optional<User> optUser = userRepository.findByEmail(request.email());
+        if(optUser.isPresent()) {
+            User foundUser = optUser.get();
+            if (request.password().equals(foundUser.getPassword())) {
+                return new LoginResponseDto(true, jwtUtil.generateAccessToken());
+            }
+        }                  
+        return new LoginResponseDto(false, null);
+    }
 }
