bfca6ed94587848ba035cc454604696ba6a206dd
JangIkhwan <bluensky0213@gmail.com>
2025-06-19 16:56:55 +0900

    [Feat] #5 UserController 추가

---
+++ src/main/java/com/photodiary/backend/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.photodiary.backend.user.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("users")
+@RestController
+public class UserController {
+
+}
