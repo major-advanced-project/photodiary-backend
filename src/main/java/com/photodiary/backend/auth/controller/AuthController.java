d409aaf7b1935c953307c7a8a8e62cab9275f8d2
JangIkhwan <bluensky0213@gmail.com>
2025-06-19 16:44:18 +0900

    [Feat] #5 컨트롤러에 로그인 관련 예외처리 구현

--- src/main/java/com/photodiary/backend/auth/controller/AuthController.java
+++ src/main/java/com/photodiary/backend/auth/controller/AuthController.java
@@ -1,8 +1,8 @@
 package com.photodiary.backend.auth.controller;
 
 import com.photodiary.backend.auth.dto.LoginRequestDto;
-import com.photodiary.backend.auth.dto.LoginResponseDto;
 import com.photodiary.backend.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,14 +10,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @Slf4j
 @RequestMapping("auth")
+@RequiredArgsConstructor
 @RestController
 public class AuthController {
     private final AuthService authService;
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto request){
+    public ResponseEntity<Object> login(@RequestBody LoginRequestDto request){
         log.info("[login]");
-        return ResponseEntity.ok(authService.login(request));
+        try{
+            return ResponseEntity.ok(authService.login(request));
+        }
+        catch(RuntimeException e){
+            log.error("error message = {}", e.getMessage());
+            HashMap<Object, Object> body = new HashMap<>();
+            body.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(body);
+        }
     }
 }
