ea94c23abb06605fd04842f977de528eb963ca01
JangIkhwan <bluensky0213@gmail.com>
2025-06-19 17:08:45 +0900

    [Feat] #5 UserService 추가

---
+++ src/main/java/com/photodiary/backend/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.photodiary.backend.user.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class UserService {
+
+}
