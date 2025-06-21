d8e931e46727c899f4e9ae776db1067ab1687b0e
JangIkhwan <bluensky0213@gmail.com>
2025-06-21 16:17:21 +0900

    [Feat] #3 SaveDiaryService 추가

---
+++ src/main/java/com/photodiary/backend/diary/service/SaveDiaryService.java
@@ -0,0 +1,17 @@
+package com.photodiary.backend.diary.service;
+
+import com.photodiary.backend.diary.dto.SaveDiaryRequestDto;
+import com.photodiary.backend.global.exception.UserNotFoundException;
+import com.photodiary.backend.user.model.User;
+import com.photodiary.backend.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class SaveDiaryService {
+
+}
