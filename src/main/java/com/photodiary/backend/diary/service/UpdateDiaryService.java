bb0f284e1a8da23320ce246963e84d39ccf1ce75
JangIkhwan <bluensky0213@gmail.com>
2025-06-21 15:43:02 +0900

    [Feat] #18 서비스에 일기 수정 로직 작성

--- src/main/java/com/photodiary/backend/diary/service/UpdateDiaryService.java
+++ src/main/java/com/photodiary/backend/diary/service/UpdateDiaryService.java
@@ -1,9 +1,29 @@
 package com.photodiary.backend.diary.service;
 
+import com.photodiary.backend.diary.dto.UpdateDiaryRequestDto;
+import com.photodiary.backend.diary.exception.UserDiaryNotFoundException;
+import com.photodiary.backend.diary.model.Diary;
+import com.photodiary.backend.diary.repository.DiaryRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class UpdateDiaryService {
+
+    private final DiaryRepository diaryRepository;
+
+    @Transactional
+    public void updateDiary(long userId, long diaryId, UpdateDiaryRequestDto request) {
+        Diary diary = diaryRepository.findById(diaryId).orElseThrow(() -> new UserDiaryNotFoundException("존재하지 않는 일기입니다"));
+
+        if(!diary.isWriter(userId)){
+            throw new UserDiaryNotFoundException("존재하지 않는 일기입니다");
+        }
+
+        diary.update(request.title(), request.content());
+    }
 }
