d171c6b1042679ba7203885968b6a0dcb03ee7de
JangIkhwan <bluensky0213@gmail.com>
2025-06-22 23:25:34 +0900

    [Fix] #31 허용할 메소드에 PATCH 추가

--- src/main/java/com/photodiary/backend/global/config/WebConfig.java
+++ src/main/java/com/photodiary/backend/global/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로
                 .allowedOrigins("http://localhost:3000", "https://ku-photodiary.vercel.app") // 프론트 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
