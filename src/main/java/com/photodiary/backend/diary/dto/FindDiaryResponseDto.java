b8f183e6fc04d475b3bd2fb6ed1de51eb04ca428
김민석 <skyfox001015@naver.com>
2025-06-22 19:41:42 +0900

    [Feat] #29 생성일, 유저이름 추가

--- src/main/java/com/photodiary/backend/diary/dto/FindDiaryResponseDto.java
+++ src/main/java/com/photodiary/backend/diary/dto/FindDiaryResponseDto.java
@@ -4,25 +4,30 @@ import com.photodiary.backend.diary.model.Diary;
 import com.photodiary.backend.diary.model.Image;
 import lombok.Builder;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Builder
 public record FindDiaryResponseDto(
         Long diaryId,
+        String username,
         String title,
         String content,
         List<ImageResponseDto> images,
-        boolean isPublic
+        boolean isPublic,
+        LocalDateTime createdAt
 
 ) {
     public static FindDiaryResponseDto entityToDto(Diary diary){
         return FindDiaryResponseDto.builder()
                 .diaryId(diary.getId())
+                .username(diary.getUser().getUsername())
                 .title(diary.getTitle())
                 .content(diary.getContent())
                 .images(diary.getImages().stream().map(ImageResponseDto::entityToDto).collect(Collectors.toList()))
                 .isPublic(diary.isPublic())
+                .createdAt(diary.getCreatedAt())
                 .build();
     }
 }
