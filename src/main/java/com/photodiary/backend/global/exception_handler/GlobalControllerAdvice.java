a95d18c590aeac8a9b240f13c4c5d5a74a6d756c
김민석 <skyfox001015@naver.com>
2025-06-23 12:01:26 +0900

    [HotFix] import 추가

--- src/main/java/com/photodiary/backend/global/exception_handler/GlobalControllerAdvice.java
+++ src/main/java/com/photodiary/backend/global/exception_handler/GlobalControllerAdvice.java
@@ -3,8 +3,8 @@ package com.photodiary.backend.global.exception_handler;
 import com.photodiary.backend.global.exception.JwtAuthenticationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
